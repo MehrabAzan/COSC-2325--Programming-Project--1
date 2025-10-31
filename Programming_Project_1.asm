97781238099a9b7771d84659da7be2bc60e9670e
Mehrab Azan <77406365+MehrabAzan@users.noreply.github.com>
2025-10-31 02:48:11 -0500

    Update Programming_Project_1.asm

--- Programming_Project_1.asm
+++ Programming_Project_1.asm
@@ -1,29 +1,31 @@
 .data
+# prompts and messages
 prompt_n: .asciiz "Enter a number: "
 prompt_b: .asciiz "Enter a base: "
+result_msg: .asciiz "Result: "
 
 .text
 .globl main
 main:
     Error:
-    # ask for user input for number
+    # print prompt_n
     li   $v0, 4
     la   $a0, prompt_n
     syscall
+    # read number input
     li   $v0, 5
     syscall
     # load number into $t0
     move $t0, $v0
-    # ask for user input for base
+    # print prompt_b
     li   $v0, 4
     la   $a0, prompt_b
     syscall
+    # read base input
     li   $v0, 5
     syscall
     # load base into $t1
     move $t1, $v0
-    li   $v0, 10
-    syscall
     # result
     li $t2, 0
     # tracks successive powers of the base
@@ -32,12 +34,13 @@ main:
     blt $t1, 2, Error
     bgt $t1, 10, Error
     Loop:
-    bgt $t0, 0, After
+    # exit once number is 0
+    beq  $t0, $zero, After
     # get the last digit
     remu $t4, $t0, 10
     # check if digit is valid in the given base
     bge $t4, $t1, Error
-    # add digit * (base ^ position) to result
+    # add digit * base^position to result
     mul $t4, $t4, $t3
     add $t2, $t2, $t4
     # move to next power of base
@@ -46,3 +49,14 @@ main:
     divu $t0, $t0, 10
     j Loop
     After:
+    # print result message
+    li   $v0, 4
+    la   $a0, result_msg
+    syscall
+    # print value in $t2
+    li   $v0, 1
+    move $a0, $t2
+    syscall
+    # exit
+    li   $v0, 10
+    syscall
